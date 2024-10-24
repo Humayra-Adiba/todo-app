96def4aa6dcd58987860299daff7d9fd47f6a029
Humayra-Adiba <tahsinadiba2024@outlook.com>
2024-10-24 11:27:22 +0600

    append todo items

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ addTodoButton.addEventListener('click', function() {
         newTodoInput.value = '';
     }
 });
+ 
+
+  // Function to create a to-do item
+  function addTodoItem(text) {
+    const li = document.createElement('li');
+    li.className = 'flex justify-between items-center p-2 border-b';
+
+    const span = document.createElement('span');
+    span.textContent = text;
+
+    const editButton = document.createElement('button');
+    editButton.innerHTML = 'Edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+
+    const doneButton = document.createElement('button');
+    doneButton.innerHTML = 'Done';
+  }
